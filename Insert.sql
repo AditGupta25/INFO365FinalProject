eb4bb5c378b5170fc8ced39d09e5226b872c9d3b
tobio500 <34556410+tobio500@users.noreply.github.com>
2017-12-15 20:16:46 -0500

    Add files via upload

--- Insert.sql
+++ Insert.sql
@@ -32,6 +32,29 @@ INSERT INTO Coach values (0066,'Scott', 'Byron', 800000);
 INSERT INTO Coach values (0077,'Stevens', 'Brad', 770000);
 
 
+
+Create table Team_Record (
+	RecordID			NUMBER(4),
+	TeamID 				NUMBER(4),
+	Wins				NUMBER(3) NOT NULL,
+	Losses				NUMBER(3) NOT NULL,
+	CoachID				NUMBER(4) NOT NULL,
+	SeasonID 			NUMBER(4) NOT NULL,
+	CONSTRAINT pk_recordid
+	PRIMARY KEY (RecordID),
+	CONSTRAINT fk_teamid
+		FOREIGN KEY (TeamID)
+		REFERENCES Team(TeamID)
+		ON DELETE CASCADE,
+	CONSTRAINT fk_recordszn
+		FOREIGN KEY (SeasonID)
+		REFERENCES Season(SeasonID)
+		ON DELETE CASCADE,
+	CONSTRAINT fk_teamcoach
+		FOREIGN KEY (CoachID)
+		REFERENCES Coach(CoachID)
+		ON DELETE CASCADE
+
 --RECORDS		
 INSERT INTO Team_Record values (1000,0001,27,55,0011,2008);
 INSERT INTO Team_Record values (1100,0001,24,58,0011,2009);	
@@ -90,46 +113,113 @@ INSERT INTO Team_Record values (5900,0005,35,47,0055,2017);
 		
 --PLAYERS
 
-Create table Player (
+INSERT INTO Player values (5000,'Paul','Chris','PG');
+INSERT INTO Player values (5001,'Thompson','Klay','SG');
+INSERT INTO Player values (5002,'James','LeBron','F');
+INSERT INTO Player values (5003,'Griffin','Blake','F');
+INSERT INTO Player values (5004,'Porzingis','Kristaps','C');
+
+INSERT INTO Player values (5005,'Irving','Kyree','PG');
+INSERT INTO Player values (5006,'Harden','James','G');
+INSERT INTO Player values (5007,'George','Paul','F');
+INSERT INTO Player values (5008,'Horford','Al','F');
+INSERT INTO Player values (5009,'McHale','Kevin','C');		
+		
+INSERT INTO Player values (5010,'Curry','Stephen','PG');
+INSERT INTO Player values (5011,'Jordan','Michael','SG');
+INSERT INTO Player values (5012,'Butler','Jimmy','G');
+INSERT INTO Player values (5013,'Love','Kevin','F');
+INSERT INTO Player values (5014,'Cousins','DeMarcus','C');		
+		
+INSERT INTO Player values (5015,'Iverson','Allen','PG');
+INSERT INTO Player values (5016,'Beal','Bradley','SG');
+INSERT INTO Player values (5017,'Cheeks','Mo','F');
+INSERT INTO Player values (5018,'Igoudala','Andre','F');
+INSERT INTO Player values (5019,'Embid','Joel','C');	
+
+INSERT INTO Player values (5016,'Westbrook','Russell','PG');
+INSERT INTO Player values (5017,'Bryant','Kobe','SG');
+INSERT INTO Player values (5018,'Leonard','Kawai','F');
+INSERT INTO Player values (5019,'Durant','Kevin','F');
+INSERT INTO Player values (5020,'Jordan','DeAndre','C');	
+
+Create table Stats (
+	StatID 				NUMBER(4),
 	PlayerID 			NUMBER(4),
-	Lname 				VARCHAR2(20),
-	Fname 			 	VARCHAR2(20),
-	Position			VARCHAR(2),
-	CONSTRAINT pk_player
-	PRIMARY KEY (PlayerID)
-	CONSTRAINT playerteam
-		FOREIGN KEY (Team)
-		REFERENCES Team(RecordID)
+	RecordID			NUMBER(4),
+	GamesPlayed 		NUMBER(3),
+	Points 				NUMBER(4),
+	Assists 			NUMBER(4),
+	Rebounds 			NUMBER(4),
+	CONSTRAINT pk_stat
+	PRIMARY KEY (StatID),
+	CONSTRAINT fk_playerrecord
+		FOREIGN KEY (RecordID)
+		REFERENCES Team_Record(RecordID)
 		ON DELETE CASCADE
+);	
+
+
+----------NY PLAYER STATS----------------------------
+--Chris Paul
+INSERT INTO Stats values (6000,5000,2000,82,990,580,100);
+INSERT INTO Stats values (6001,5000,2100,82,1315,550,125);
+INSERT INTO Stats values (6002,5000,2200,82,1392,700,140);
+INSERT INTO Stats values (6003,5000,2300,82,1504,745,147);
+INSERT INTO Stats values (6004,5000,2400,82,1551,710,130);
+INSERT INTO Stats values (6005,5000,2500,82,1642,770,111);
+INSERT INTO Stats values (6006,5000,2600,82,1670,811,140);
+INSERT INTO Stats values (6007,5000,2700,82,1689,1200,132);
+INSERT INTO Stats values (6008,5000,2800,82,1569,1240,100);
+INSERT INTO Stats values (6009,5000,2900,82,1470,1260,110);
+
+--Klay Thompson		
+INSERT INTO Stats values (6010,5001,2000,82,1100,410,380);		
+INSERT INTO Stats values (6011,5001,2100,82,1200,475,400);	
+INSERT INTO Stats values (6012,5001,2200,82,1270,490,420);	
+INSERT INTO Stats values (6013,5001,2300,82,1660,500,440);	
+INSERT INTO Stats values (6014,5001,2400,82,1669,570,523);	
+INSERT INTO Stats values (6015,5001,2500,82,1710,510,550);		
+INSERT INTO Stats values (6016,5001,2600,82,1780,596,600);	
+INSERT INTO Stats values (6017,5001,2700,82,1723,670,523);	
+INSERT INTO Stats values (6018,5001,2800,82,1800,642,541);	
+INSERT INTO Stats values (6019,5001,2900,82,1812,644,525);			
 		
+--Lebron James		
+INSERT INTO Stats values (6020,5002,2000,82,1600,500,600);	
+INSERT INTO Stats values (6020,5002,2100,82,1640,600,675);
+INSERT INTO Stats values (6020,5002,2200,82,1810,700,700);
+INSERT INTO Stats values (6020,5002,2300,82,1895,740,730);
+INSERT INTO Stats values (6020,5002,2400,82,2000,750,751);		
+INSERT INTO Stats values (6020,5002,2500,82,2240,730,774);	
+INSERT INTO Stats values (6020,5002,2600,82,2300,790,739);
+INSERT INTO Stats values (6020,5002,2700,82,2466,820,795);
+INSERT INTO Stats values (6020,5002,2800,82,2450,785,770);
+INSERT INTO Stats values (6020,5002,2900,82,2400,774,795);			
 		
-INSERT INTO Player values (5000,'James','LeBron','F');
-INSERT INTO Player values (5001,'Irving','Kyree','PG');
-INSERT INTO Player values (5000,'Harden','James','G');
-INSERT INTO Player values (5000,'Curry','Steph','G');
-INSERT INTO Player values (5000,'Durant','Kevin','F');
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+--Blake Griffin
+INSERT INTO Stats values (6021,5003,2000,82,980,310,600);	
+INSERT INTO Stats values (6021,5003,2100,82,1160,330,670);	
+INSERT INTO Stats values (6021,5003,2200,82,1200,360,686);	
+INSERT INTO Stats values (6021,5003,2300,82,1230,310,820);	
+INSERT INTO Stats values (6021,5003,2400,82,1110,420,840);	
+INSERT INTO Stats values (6021,5003,2500,82,1400,430,860);
+INSERT INTO Stats values (6021,5003,2600,82,1450,400,900);	
+INSERT INTO Stats values (6021,5003,2700,82,1500,498,910);	
+INSERT INTO Stats values (6021,5003,2800,82,1598,500,870);	
+INSERT INTO Stats values (6021,5003,2900,82,1599,475,856);			
+
+--Kristaps		
+INSERT INTO Stats values (6022,5004,2000,82,996,365,640);
+INSERT INTO Stats values (6021,5003,2100,82,1080,400,720);	
+INSERT INTO Stats values (6021,5003,2200,82,1120,410,760);	
+INSERT INTO Stats values (6021,5003,2300,82,1160,435,770);	
+INSERT INTO Stats values (6021,5003,2400,82,1230,448,765);		
+INSERT INTO Stats values (6022,5004,2500,82,1236,491,742);
+INSERT INTO Stats values (6021,5003,2600,82,1240,430,790);	
+INSERT INTO Stats values (6021,5003,2700,82,1270,450,890);	
+INSERT INTO Stats values (6021,5003,2800,82,1300,476,885);	
+INSERT INTO Stats values (6021,5003,2900,82,1450,441,874);			
 		
 		
 		
