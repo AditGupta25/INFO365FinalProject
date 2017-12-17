9cc67762224fb84ee70f21d38cf69bd3fd0f65f5
Adit <aditgupta1@yahoo.com>
2017-12-16 23:15:18 -0500

    Added the cleanup file

---
+++ Cleanup.sql
@@ -0,0 +1,18 @@
+DROP TRIGGER Stats_PPG_After_Update;
+DROP TRIGGER Stats_APG_After_Update;
+DROP TRIGGER Stats_RPG_After_Update;
+DROP PROCEDURE most_paid_coach_details;
+DROP PROCEDURE players_with_most_points;
+DROP PROCEDURE players_with_most_assists;
+DROP PROCEDURE players_with_most_games_played;
+DROP PROCEDURE best_records;
+DROP PROCEDURE worst_records;
+DROP FUNCTION divide;
+
+
+DROP TABLE Coach; -- 4
+DROP TABLE Team; -- 6
+DROP TABLE Season; -- 5
+DROP TABLE Team_Record; -- 2
+DROP TABLE Player; -- 3
+DROP TABLE Stats; -- 1
